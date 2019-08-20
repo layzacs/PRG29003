106c0764b21a5bde765ea36c266b3436c6aa71e6
layzacs <layza.cristini@gmail.com>
2019-08-20 16:19:09 -0300

    Adiciona teste 2

--- testes/teste2.cpp
+++ testes/teste2.cpp
@@ -1,1 +1,29 @@
+// https://moodle.sj.ifsc.edu.br/mod/vpl/view.php?id=5812
+#include <stdlib.h>
+#include <string>
+#include <prglib.h>
 
+using namespace std;
+using prglib::fila;
+
+
+int main() {
+
+  fila<string> q(1000);
+
+
+  while (true) {
+    string cmd;
+    cout << "Digite o comando: "; // prompt
+    getline(cin, cmd);
+    if (cmd.empty()) {
+      break;
+    }
+    q.enfileira(cmd);
+  }
+
+  while (! q.vazia()) {
+    string cmd = q.desenfileira();
+    system(cmd.c_str());
+  }
+}

4928911f72844ebc8608a903566ef6e260cf0fc5
layzacs <layza.cristini@gmail.com>
2019-08-24 22:15:42 -0300

    Adiciona projeto 1

---
+++ projetos/projeto-1/cmake-build-debug/CMakeFiles/Bacon.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Bacon.dir/main.cpp.o"
+  "CMakeFiles/Bacon.dir/Mapa.cpp.o"
+  "Bacon.pdb"
+  "Bacon"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bacon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ projetos/projeto-1/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include "Mapa.h"
+#include <prglib.h>
+using namespace std;
+using prglib::fila;
+
+int busca_bfs() {
+    string ator;
+    string filme;
+
+    fila<string> atores(1000);
+    fila<string> filmes(1000);
+
+    cout << "Digite o nome do ator:" << endl;
+    cin >> ator;
+
+    cout << "shit happens";
+
+    Mapa dados("data/dados.txt");
+    filmes = dados.obtem_filmes("Kevin Bacon");
+
+    cout << "shit hapens again";
+
+    while (! filmes.vazia()) {
+        string umFilme = filmes.desenfileira();
+        cout << "Filme: " << umFilme << endl;
+    }
+    return 0;
+}
+
+int lista_filmes() {
+
+    Mapa mapa("../data/dados.txt");
+
+    fila<string> q = mapa.obtem_filmes(bacon);
+    int n = q.comprimento();
+
+    while (! q.vazia()) {
+        cout << q.desenfileira() << endl;
+    }
+
+    cout << bacon << " atuou em " << n << " filmes" << endl;
+    return 0;
+}
+
+int main() {
+
+    int i;
+    cout << "Distância de Bacon" << endl;
+
+    cout << "Escolha entre as opções: \n1 - Calcular a distância de Bacon de um ator por meio do algoritmo de busca em largura.\n2 - Buscar a distância de Bacon de um ator por mei do algoritmo de busca em profundidade.\n3 - Listar filmes de Kevin Bacon." << endl;
+    cin >> i;
+
+    switch (i) {
+        case 1:
+            busca_bfs();
+            break;
+
+        case 2:
+            //busca_dfs();
+            break;
+
+        case 3:
+            lista_filmes();
+            break;
+
+    }
+    return 0;
+}

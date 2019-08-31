e0a5089ebdc159989285df516920a5ea77391d5f
layzacs <layza.cristini@gmail.com>
2019-08-31 13:50:22 -0300

    Adiciona exercício de lista

--- listas/cria-lista-ordenada-inteiros-aleatorios.cpp
+++ listas/cria-lista-ordenada-inteiros-aleatorios.cpp
@@ -1,19 +1,37 @@
+#include "aleatorios.h"
+
+
+using namespace std;
+using prglib::lista;
+
 void adiciona_aleatorios(lista<int> & umaLista, int N) {
-    lista<int> l;
+    int i, j;
+    int num;
 
     srand (time(NULL));
 
-    for (int i=0; i < N; i++) {
-        int num = 1 + (rand() % 10);
-        if (i=0) {
+    for (i=0; i < N; i++) {
+
+        num = 1 + (rand() % 10);
+
+        int tam_lista = umaLista.comprimento();
+
+        if (i==0) {
             umaLista.anexa(num);
         }
-        if (i>0 && num > umaLista.obtem(i)) {
-          umaLista.anexa(num);
-        }
-        if (i>0 && num < umaLista.obtem(i)) {
-          umaLista.insere(i-1);
+        else {
+            for (j=0; j < tam_lista; j++) {
+
+                if (num < umaLista.obtem(j)) {
+                    umaLista.insere(num, j);
+
+                    break;
+                }
+            }
+
+            if (j == tam_lista) {
+                umaLista.anexa(num);
+            }
         }
     }
-
 }

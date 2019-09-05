6a24b83dd9181cc8612ec24c9b17823744c8575a
layzacs <layza.cristini@gmail.com>
2019-09-05 15:32:59 -0300

    Adiciona função dfs ao projeto 1

--- projetos/projeto-1/main.cpp
+++ projetos/projeto-1/main.cpp
@@ -19,20 +19,85 @@ struct dados_atores {
 
 int busca_dfs(string ator_procurado) {
 
-}
+    string kevin_bacon = "Kevin Bacon";
+    if (ator_procurado == kevin_bacon) {
+        cout << "A distância entre Kevin Bacon e Kevin Bacon é zero! Por favor escolha um ator DIFERENTE de Kevin Bacon." << endl;
+    }
 
+    pilha<struct dados_atores> todos_atores(1000000);
+
+    fila<string> filmes_bacon(100);
+    filmes_bacon = dados.obtem_filmes("Kevin Bacon");
+
+    while(! filmes_bacon.vazia()) {
+        string filme_pai = filmes_bacon.desenfileira();
+
+        fila<string> atores_pai = dados.obtem_atores(filme_pai);
+
+        while(! atores_pai.vazia()) {
+            string ator_pai = atores_pai.desenfileira();
+
+            struct dados_atores id_pai;
+            id_pai.ator = ator_pai;
+            id_pai.caminho = kevin_bacon + " / " + filme_pai + " / " + ator_pai;
+            id_pai.distancia_bacon = 1;
+
+            if (ator_pai == ator_procurado) {
+                cout << "O ator " << ator_pai << " foi encontrado!" << endl;
+                cout << "Sua distância de Bacon é: " << id_pai.distancia_bacon << endl;
+                cout << id_pai.caminho << endl;
+                return 0;
+            }
+
+            todos_atores.push(id_pai);
+        }
+    }
+
+    while (! todos_atores.vazia()) {
+        struct dados_atores id_filho = todos_atores.pop();
+        string ator_filho = id_filho.ator;
+
+        fila<string> filmes_filho = dados.obtem_filmes(ator_filho);
+
+        while(! filmes_filho.vazia()) {
+            string filme_filho = filmes_filho.desenfileira();
+            fila<string> atores_neto = dados.obtem_atores(filme_filho);
+
+            while(! atores_neto.vazia()) {
+
+                string ator_neto = atores_neto.desenfileira();
+
+                struct dados_atores id_neto;
+                id_neto.ator = ator_neto;
+                id_neto.caminho = id_filho.caminho + " / " + filme_filho + " / " + ator_neto;
+                id_neto.distancia_bacon = id_filho.distancia_bacon + 1;
+
+                if (ator_neto == ator_procurado) {
+                    cout << "O ator " << ator_neto << " foi encontrado!" << endl;
+                    cout << "Sua distância de Bacon é: " << id_neto.distancia_bacon << endl;
+                    cout << "Caminho feito: " << id_neto.caminho << endl;
+                    return 0;
+                }
+
+                todos_atores.push(id_neto);
+            }
+        }
+
+    }
+    cout << "Este ator não existe ou nunca fez um filme com o Kevin Bacon. :(" << endl;
+    return 0;
+}
 
 int busca_bfs(string ator_procurado) {
 
     string kevin_bacon = "Kevin Bacon";
-
-
     if (ator_procurado == kevin_bacon) {
         cout << "A distância entre Kevin Bacon e Kevin Bacon é zero! Por favor escolha um ator DIFERENTE de Kevin Bacon." << endl;
     }
-    fila<struct dados_atores> todos_atores(1000000);
-    fila<string> filmes_bacon(100);
 
+    fila<struct dados_atores> todos_atores(1000000);
+
+    fila<string> filmes_bacon(100);
     filmes_bacon = dados.obtem_filmes("Kevin Bacon");
 
     while(! filmes_bacon.vazia()) {
@@ -99,7 +164,7 @@ int main() {
     int i;
     cout << "Distância de Bacon" << endl;
 
-    cout << "Escolha entre as opções: \n1 - Calcular a distância de Bacon de um ator por meio do algoritmo de busca em largura.\n2 - Buscar a distância de Bacon de um ator por mei do algoritmo de busca em profundidade.\n3 - Listar filmes de Kevin Bacon." << endl;
+    cout << "Escolha entre as opções: \n1 - Calcular a distância de Bacon de um ator por meio do algoritmo de busca em largura.\n2 - Buscar a distância de Bacon de um ator por mei do algoritmo de busca em profundidade.\n3 - Sair." << endl;
     cin >> i;
     cin.ignore();
 
